f82cfa2f40d3cd00ecd42916239d3c8b780c0c53
hemanath1112 <hemanath.chakkaravarthi@gmail.com>
2023-08-17 01:11:19 +0530

    updated

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const mov = document.querySelector('.top');
 
 window.addEventListener('scroll', () => {
     var y = window.scrollY;
-    if (y >= 400) {
+    if (y >= 600) {
         mov.classList.add('hema');
     } else {
         mov.classList.remove('hema');
